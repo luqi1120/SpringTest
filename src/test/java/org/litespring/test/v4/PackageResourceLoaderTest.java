c6a2080fc01e7748b26d1891cad04c4a94b82d0f
luqi1120 <luqi_159159@126.com>
2018-07-23 16:58:12 +0800

    实现自定义注解，第一步把package下面的class变成resource

---
+++ src/test/java/org/litespring/test/v4/PackageResourceLoaderTest.java
@@ -0,0 +1,23 @@
+package org.litespring.test.v4;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.litespring.core.io.Resource;
+import org.litespring.core.io.support.PackageResourceLoader;
+
+/**
+ * 实现自定义注解，第一步把package下面的class变成resource
+ */
+public class PackageResourceLoaderTest {
+
+	@Test
+	public void testGetResources() throws IOException{
+		PackageResourceLoader loader = new PackageResourceLoader();
+		Resource[] resources = loader.getResources("org.litespring.dao.v4");
+		Assert.assertEquals(2, resources.length);
+		
+	}
+
+}
